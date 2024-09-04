51a0a444de555061fad40eeac8be6fa30de0eb06
EduGA457 <eduardo_3041230367@utd.edu.mx>
2024-09-03 19:49:53 -0600

    diagnositico

---
+++ script.js
@@ -0,0 +1,15 @@
+/* Pregunta: ¿Cuál es la función de 'document.getElementById' en JavaScript? utilizar un elemento de tu codigo mediante su id*/
+function checkAnswer() {
+    /* Pregunta: ¿Qué hace 'prompt' y cómo se puede usar en lugar de 'alert'? al alert se muetra coomo una notificacion y el promt en la pantalla */
+    let answer = prompt("Enter your answer:");
+
+    /* Pregunta: ¿Cuál es el propósito de la instrucción 'if' en este fragmento de código? ejecutar lineas de codigo dependiendo si se cumple cierta condicion */
+    if (answer.toLowerCase() === 'paris') {
+        alert("Correct!");
+    } else {
+        alert("Try again!");
+    }
+}
+
+/* Pregunta: ¿Cómo se puede manipular el DOM usando JavaScript para cambiar el contenido de un elemento? */
+document.getElementById("question").innerText = "What is the capital of Spain?";
